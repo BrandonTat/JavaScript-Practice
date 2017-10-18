96e38263cb08cd380844ce78c58215e90e1f9b8e
Brandon Tat <brandontat7@gmail.com>
2017-10-18 00:30:49 -0700

    add structure for tabs

---
+++ React/widgets/frontend/tabs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Tabs extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {selected: 0};
+  }
+
+  render(){
+    return(
+      <div>
+        tabs
+      </div>
+    );
+  }
+}
+
+export default Tabs;
--- React/widgets/frontend/widget.jsx
+++ React/widgets/frontend/widget.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const Panes = [
+  {title: "one", content:"I am the first"},
+  {title: "two", content:"I am the second"},
+  {title: "three", content:"I am the last"}
+];
 
 class Root extends React.Component {
   render() {
     return(
       <div>
         <Clock />
+        <Tabs panes={Panes}/>
       </div>
     );
   }
